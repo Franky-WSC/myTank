888487c81d185e713855593ef49c399b0ce2c27d
Franky <wangshengchao@nuaa.edu.cn>
2022-02-04 12:15:01 +0800

    去掉敌方坦克的初始化部分, 为网络版做准备

--- src/com/cetc28/tank/Main.java
+++ src/com/cetc28/tank/Main.java
@@ -14,12 +14,14 @@ public class Main {
     // 这是程序的main函数:入口函数
     public static void main(String[] args) {
         TankFrame tf = new TankFrame();
-        //读取配置文件
-        int tankCount = PropertyMgr.getInt("initTankCount");
-        //初始化敌方坦克
-        for (int i = 0; i < tankCount; i++) {
-            tf.tanks.add(new Tank(50+i*100,400,Dir.DOWN, Group.BAD, tf));
-        }
+//        //读取配置文件
+//        int tankCount = PropertyMgr.getInt("initTankCount");
+//        //初始化敌方坦克
+//        for (int i = 0; i < tankCount; i++) {
+//            tf.tanks.add(new Tank(50+i*100,400,Dir.DOWN, Group.BAD, tf));
+//        }
+
+        new Thread(()->new Audio("audio/war1.wav").loop()).start();
 
         while (true){
             try {
